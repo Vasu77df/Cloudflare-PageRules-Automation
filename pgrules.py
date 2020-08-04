9899e04ac99d1b2839e1c836554244f8b103b402
Vasu77df <vasu3797@gmail.com>
2020-08-04 11:22:18 -0500

    proof of concept for setting pgrules

---
+++ pgrules.py
@@ -0,0 +1,44 @@
+import json 
+import requests
+import logging 
+import pprint
+
+logging.basicConfig(filename='PGRULES.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+def user_details(api_key, acc_email):
+    response = requests.get("https://api.cloudflare.com/client/v4/user", headers = {'X-Auth-Email': acc_email, 'X-Auth-Key': api_key, 'Content-Type': 'application/json'})
+    user_dets = json.loads(response.text)
+    return user_dets
+
+def list_zones(api_key, acc_email):
+    response = requests.get("https://api.cloudflare.com/client/v4/zones", headers = {'X-Auth-Email': acc_email, 'X-Auth-Key': api_key, 'Content-Type': 'application/json'})
+    zones = json.loads(response.text)
+    return zones
+
+def list_pgrules(api_key, acc_email, zone_id):
+    response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/pagerules", headers = {'X-Auth-Email': acc_email, 'X-Auth-Key': api_key, 'Content-Type': 'application/json'})
+    pgrules = json.loads(response.text)
+    return pgrules
+
+def set_pgrules(api_key, acc_email, zone_id, domain_url, cache_param):
+    for i in cache:
+        param = domain_url + i
+        data = {"targets":[{"target":"url","constraint":{"operator":"matches","value": param}}],"actions":[{"id":"always_online","value":"on"}],"priority":1,"status":"active"}
+        response = requests.get(f"https://api.cloudflare.com/client/v4/zones/{zone_id}/pagerules", data=data, headers = {'X-Auth-Email': acc_email, 'X-Auth-Key': api_key, 'Content-Type': 'application/json'})
+    
+    return response
+
+def main():
+    with open('credentials.json') as json_file:
+        pp = pprint.PrettyPrinter(indent=4)
+        data = json.load(json_file)
+        api_key = data["API_KEY"]
+        acc_email = data["ACC_EMAIL"]
+        acc_id = data["ACC_ID"]
+        zone_id = data["ZONE_ID"]
+        domain_url = data["DOMAIN_URL"]
+        dets = user_details(api_key, acc_email)
+        print(json.dumps(dets, indent=4))
+        
+if __name__ == "__main__":
+    main()
+
--- pgrules.sh
+++ pgrules.sh
@@ -1,9 +1,37 @@
 export API_KEY=21cb77cdedae9c7c58ca58dacac0d9e00e043
 export ACC_EMAIL=vasu3797@gmail.com
-export DOMAIN_ZONE=xyz # fill in in Domain of the endpoint domain
+export ACCOUNT_ID=48773ba1335af750ade5f50033b70ed4 
+export ZONE_ID=xyz
+export DOMAIN_URL=example.com
+# Getting User Details 
+curl -X GET "https://api.cloudflare.com/client/v4/user" \
+    -H "X-Auth-Email: $ACC_EMAIL" \
+    -H "X-Auth-Key: $API_KEY" \
+    -H "Content-Type: application/json" | python3 -m json.tool
 
-curl -X GET "https://api.cloudflare.com/client/v4/user" -H "X-Auth-Email: $ACC_EMAIL" -H "X-Auth-Key: $API_KEY" \
-    -H "Content-Type: application/json" >> output.json
+# Getting Zones
+curl -X GET "https://api.cloudflare.com/client/v4/zones" \
+    -H "X-Auth-Email: $ACC_EMAIL" \
+    -H "X-Auth-Key: $API_KEY" \
+    -H "Content-Type: application/json" | python3 -m json.tool
+
+# Listing All Page Rules 
+curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/pagerules" \
+    -H "X-Auth-Email: $ACC_EMAIL" \
+    -H "X-Auth-Key: $API_KEY" \
+    -H "Content-Type: application/json" | python3 -m json.tool
+
+# Creating a Page Rule
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/pagerules" \
+    -H "X-Auth-Email: $ACC_EMAIL" \
+    -H "X-Auth-Key: $API_KEY" \
+    -H "Content-Type: application/json" \
+    --data '{"targets":[{"target":"url","constraint":{"operator":"matches","value":"*example.com/*.jpg*"}}],"actions":[{"id":"always_online","value":"on"}],"priority":1,"status":"active"}'
+
+
+
+
+
 
 
 
